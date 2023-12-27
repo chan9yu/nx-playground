d5dfc3049596e0ab8025fd3497d737d5e2e30cc0
chan9yu <wry5533@gmail.com>
2023-12-27 15:39:53 +0900

    docs: nextra docs 구성

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@nx-playground/react-components';
+import '@nx-playground/react-components/style.css';
+import '@nx-playground/theme/themes.css';
+
+export default {
+	logo: <Heading fontSize="xl">nx-playground</Heading>,
+	project: {
+		link: 'https://github.com/chan9yu/nx-playground/'
+	}
+};
